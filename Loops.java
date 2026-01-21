0125246651c988e809e1f318ad41ab03ef6b80f7
Isaac Alexander <Isaacalexander81@gmail.com>
2026-01-21 02:00:48 +0100

    Loops.java

--- Loops.java
+++ Loops.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Loops {
+    public static void main(String[] args) {
+        // For Loops
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
+
+        //While Loops
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello World " + i);
+            i++;
+        }
+
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+
+        // Do... While Loops
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+    }
 }
