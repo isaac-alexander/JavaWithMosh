7e75b3bc7c4bfbc3d007a8c1cf0af1ee3ae625b9
Isaac Alexander <Isaacalexander81@gmail.com>
2026-01-22 02:34:14 +0100

    ForEachLoops.java

--- ForEachLoops.java
+++ ForEachLoops.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ForEachLoops {
+    public static void main(String[] args) {
+        // For-Each Loop
+        String[] fruits = { "Apple", "Mango", "Orange"};
+
+        for (int index = 0; index < fruits.length; index++) {
+            System.out.println(fruits[index]);
+        }
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
 }
